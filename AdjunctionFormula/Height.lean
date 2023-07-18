f2fb84088f1006e6285a99dd0b284c038969a99d
JJ Garzella <jjgarzella@gmail.com>
2023-07-17 22:06:40 -0700

    Add some statements about heights
    Also add a #mwe for a zulip question
    that should be removed later.

--- AdjunctionFormula/Height.lean
+++ AdjunctionFormula/Height.lean
@@ -1,38 +1,70 @@
-import Mathlib.RingTheory.Ideal.basic
+-- import Mathlib.RingTheory.Ideal.basic
+import Mathlib.AlgebraicGeometry.PrimeSpectrum.Maximal
+import Mathlib.RingTheory.Ideal.MinimalPrime
 
-
-namespace Height
+namespace Ideal
 
 variable {R : Type _} [CommRing R]
 
-def Height {p : Ideal R} (hp : p.IsPrime) : Int := sorry
+---def Height {p : Ideal R} (hp : p.IsPrime) : Int := sorry
 -- sup of lengths of chains of prime ideals
 
 --this is actually in mathlib for preorders or something, but remember to take the
 --set of PRIME ideals, not the set of ideals
 
-def Height (I : Ideal R) : Int := sorry
--- sup of heights of primes containing this
+noncomputable def height (I : Ideal R) : WithBot (WithTop ℕ) := ⨆ (p : I.minimalPrimes), sorry--p.height
+-- sup of heights of primes containing this ideal
+
+lemma height_eq_prime_height (I : Ideal R) [IsPrime I] :
+height I = I.height := sorry
 
 --this is not already in mathlib
 
---lemma height_is_sup (I : Ideal R) :
---(Height I = Sup {lengths of chains of primes containing I}) := sorry
 
-end Height
+end Ideal
 
-namespace Dim
+namespace Ring
 
-variable (R : Type _) [CommRing R]
+noncomputable def dim (R : Type _) [CommRing R] : WithBot (WithTop ℕ) := ⨆ (m : MaximalSpectrum R), (height m)
+-- sup of heights of all primes in R (i.e. the krull dim of the maximal spectrum)
+-- also in mathlib for preorders, I think
 
-def Dim := sorry
--- sup of heights of all primes in R
+end Ring
 
--- also in mathlib for preorders
+namespace Dimension
 
---lemma Dim_eq_sup_heights_maximal (Dim R) = ⨆ {m: maximal ideals}, m.Height
+open Ring
+open Ideal
 
---corollary : if R is local, dimension is equal to the height of the maximal ideal
---corollary : dimension of R_p is the same as the height of p
+variable {R : Type _} [CommRing R]
 
-end Dim
+lemma dim_eq_sup_heights_maximal :
+(dim R) = (⨆ (m : (MaximalSpectrum R)), (height m.asIdeal)) :=
+sorry
+
+-- this is a corollary, not a theorem... where did the keyword go??
+theorem dim_eq_height_of_maximal_of_LocalRing [LocalRing R] :
+(dim R) = (height (LocalRing.maximalIdeal R)) := sorry
+
+-- this is a corollary, not a theorem... where did the keyword go??
+-- the dimension of the local ring at p is the same as the height of p
+theorem dim_localized_at_p_eq_height_p (p : Ideal R) [IsPrime p] :
+(dim (Localization.AtPrime p)) = (height p) := sorry
+-- note bug in docs of line 67-68 of Mathlib/RingTheory/AtPrime.lean???, I vs P, R vs S
+
+variable (y : WithTop (WithBot ℕ))
+
+#check (↑y) - (1 : WithTop (WithBot ℤ))
+
+
+-- theorem dim_minus_1_of_quotient_element_non_principal (x : R)
+-- (hx : ∀ p ∈ (⊥ : Ideal R).minimalPrimes, x ∉ p) :
+-- (dim (R ⧸ {x})) ≤ (↑(dim R) : WithBot (WithTop ℤ)) - (1 : WithBot (WithTop ℤ)) := sorry
+
+-- the theorem about quotienting by an element in a non-minimal prime decreases height by
+--   (at least) one
+
+-- Krull's height theorem can go here (along with the prinicipal ideal theorem)
+
+end Dimension
+--↥↑', '↥', '⇑
